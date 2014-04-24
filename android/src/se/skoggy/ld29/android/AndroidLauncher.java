fb9179909896d760377f10f51ccc2b9222a121bd
ErikS <erikskoglund88@gmail.com>
2014-04-24 17:50:30 +0100

    init

---
+++ android/src/se/skoggy/ld29/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package se.skoggy.ld29.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import se.skoggy.ld29.LD29;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new LD29(), config);
+	}
+}
---
+++ desktop/src/se/skoggy/ld29/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package se.skoggy.ld29.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import se.skoggy.ld29.LD29;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LD29(), config);
+	}
+}
